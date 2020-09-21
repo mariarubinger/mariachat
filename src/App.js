f21690c428ede2be503e4ef5dc971f960e33b26a
Maria Rubinger <rubinger.maria@gmail.com>
2020-09-21 10:54:12 -0300

    Criação de interface

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import ChatListItem from './components/ChatListItem';
+import ChatIntro from './components/ChatIntro';
+import ChatWindow from './components/ChatWindow';
 
-export default App;
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+export default () => {
+
+    const [chatlist, setChatList] = useState([
+        {chatId: 1, title: 'Maria Clara', image: 'https://www.w3schools.com/w3images/avatar5.png'},
+        {chatId: 2, title: 'Maria Clara', image: 'https://www.w3schools.com/w3images/avatar5.png'},
+        {chatId: 3, title: 'Maria Clara', image: 'https://www.w3schools.com/w3images/avatar5.png'},
+        {chatId: 4, title: 'Maria Clara', image: 'https://www.w3schools.com/w3images/avatar5.png'},
+    ]);
+    const [activeChat, setActiveChat] = useState({});
+
+    return (
+        <div className="app-window">
+            <div className="sidebar">
+                
+                <header>
+                    <img className="header--avatar" src="https://www.w3schools.com/w3images/avatar5.png" alt="" />
+                    <div className="header--buttons">
+                        <div className="header--btn">
+                            <DonutLargeIcon style={{color: '#919191'}} />
+                        </div>
+                        <div className="header--btn">
+                            <ChatIcon style={{color: '#919191'}} />
+                        </div>
+                        <div className="header--btn">
+                            <MoreVertIcon style={{color: '#919191'}} />
+                        </div>
+                    </div>
+                </header>
+
+                <div className="search">
+                    <div className="search--input">
+                        <SearchIcon fontSize="small" style={{color: '#919191'}}/>
+                        <input type="search" placeholder="Procurar ou começar uma nova conversa"></input>
+                    </div>
+                </div>
+
+                <div className="chatlist">
+                    {chatlist.map((item, key) =>(
+                        <ChatListItem
+                            key={key}
+                            data={item}
+                            active={activeChat.chatId === chatlist[key].chatId}
+                            onClick={()=>setActiveChat(chatlist[key])}
+                        />
+                    ))}
+                </div>
+
+            </div>
+            <div className="contentarea">
+                {activeChat.chatId !== undefined &&
+                    <ChatWindow />
+                }
+                {activeChat.chatId === undefined &&
+                    <ChatIntro />
+                }
+            </div>
+        </div>
+    );
+}
